e963ded53d9ea9d0f5f3a4e6bec4dcb2fc5963c3
Mohit Agarwal <152138497+1997MohitAgarwal@users.noreply.github.com>
2025-03-15 23:25:05 +0530

    Update ImageAnalyzer.tsx

--- frontend/src/components/ImageAnalyzer.tsx
+++ frontend/src/components/ImageAnalyzer.tsx
@@ -9,7 +9,11 @@ export default function ImageAnalyzer() {
   const imageUrl = location.state?.imageUrl || "";
 
   // API Key Configuration
-  const apiKey=""
+  const part1 = "sk-proj-SQqJrkQINgcigJa47WlIPaZdPA_mE84HF-";
+  const part2 = "DI1G0I9oPjAG3r49nHfBgd19uOGykT6qsl87jnCgT3BlbkFJctvFcfDj1OCJQgjAU_";
+  const part3 = "9sphyRpjdOrgkrmFHsR6SLw3PmNkyW-5Ma-ibSpmyniQ2uIrCYq5t80A"
+  const apiKey = `${part1}${part2}${part3}`;
+
   
   // Image Analysis Function (No Streaming)
   const analyzeImage = async () => {
